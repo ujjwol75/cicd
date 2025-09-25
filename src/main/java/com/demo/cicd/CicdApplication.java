47dae3b7d7611cb038b7d434a8e6a0040a75016a
ujjwol75 <ujjwolneupane75@gmail.com>
2025-09-25 19:53:27 +0545

    main class changes

--- src/main/java/com/demo/cicd/CicdApplication.java
+++ src/main/java/com/demo/cicd/CicdApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CicdApplication {
 
 	@GetMapping
-	public String message												() {
+	public String message() {
 		return "Hello Ujjwol Neupane. What's up ? ";
 	}
 
